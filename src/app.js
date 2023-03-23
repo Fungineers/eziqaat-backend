d962724c5a7ba18826133c061eced9704210e298
daniyalaamir110 <daniyal.amir110@gmail.com>
2023-03-23 20:05:00 +0500

    middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const port = process.env.port || 3001;
 
